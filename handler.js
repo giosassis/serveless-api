75e498438fdf55a5bdd75b61585ed14f9a9fcc09
Giovana Sant Anna de Assis <CIANDT\giovanaa@lnb024128bhz.ciandt.global>
2021-11-16 09:26:03 -0300

    refactor: changing from lambda to serveless and feating endpoint

---
+++ handler.js
@@ -0,0 +1,37 @@
+"use strict";
+const pacientes = [
+  { id: 1, nome: "Maria", dataNascimento: "1984-01-11" },
+  { id: 2, nome: "Joao", dataNascimento: "1983-09-16" },
+  { id: 3, nome: "Jose", dataNascimento: "1959-07-15" },
+];
+
+module.exports.listarPacientes = async (event) => {
+  console.log(event);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        pacientes,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.obterPaciente = async (event) => {
+  const { pacienteID } = event.pathParameters;
+  const paciente = pacientes.find((paciente) => paciente.id == pacienteID);
+
+  if (paciente === undefined) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: "Paciente não existe" }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(paciente, null, 2),
+  };
+};
