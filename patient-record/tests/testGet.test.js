40fb7012c25d51e2a88b222d27396195c5bc5148
Giovana Sant Anna de Assis <CIANDT\giovanaa@lnb024128bhz.ciandt.global>
2021-11-22 16:08:18 -0300

    try: fist route test

--- patient-record/tests/testGet.test.js
+++ patient-record/tests/testGet.test.js
@@ -1,4 +1,33 @@
-const chai = require("chai");
+const sinon = require("sinon");
+const proxyquire = require("proxyquire").noCallThru;
+
+
+describe("DynamoDB Mock Test", () => {
+  let AWS;
+  let getPatient;
+
+  before(() => {
+    getPatient = sinon.stub();
+
+    AWS = {
+      DynamoDB: {
+        DocumentClient: sinon.stub().returns({
+          query: getPatient,
+        }),
+      },
+    };
+  });
+
+  scriptToTest = proxyquire("../handler.js", {
+    "aws-sdk": AWS,
+  });
+});
+
+it("should return should return 200", async () => {
+  getPatient.get.withArgs(sinon.match.any).yelds(null, {statusCode: 200});
+});
+
+/*const chai = require("chai");
 const chaiHttp = require("chai-http");
 const app = require("../handler");
 
@@ -8,8 +37,8 @@ chai.use(chaiHttp);
 describe("API Patients", () => {
   describe("GET /patients", () => {
     it("should return a list of patients", (done) => {
-      chai.request("https://ri0ic4hh2m.execute-api.us-east-1.amazonaws.com/dev/patients")
+      chai.request("http://localhost:3000/dev/patients")
       .get("/patients");
     });
   });
-});
+});*/
