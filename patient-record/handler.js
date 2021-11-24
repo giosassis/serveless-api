d6b2ca356c54970538f21a9d07587b643ac5f4ab
Giovana Sant Anna de Assis <CIANDT\giovanaa@lnb024128bhz.ciandt.global>
2021-11-24 11:59:35 -0300

    ading path with tests and english version of api

---
+++ patient-record/handler.js
@@ -0,0 +1,211 @@
+"use strict";
+
+const patient = [
+  { id: 1, nome: "Mary", birthdate: 1984 - 11 - 01 },
+  { id: 2, nome: "John", birthdate: 1990 - 11 - 01 },
+  { id: 3, nome: "Josh", birthdate: 1998 - 06 - 06 },
+];
+
+const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const params = {
+  TableName: "Patients",
+};
+
+module.exports.getPacient = async (event) => {
+  try {
+    let data = await dynamoDb.scan(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data.Items),
+    };
+  } catch (err) {
+    console.error(err);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
+  }
+};
+
+module.exports.getPacientByID = async (event) => {
+  try {
+    const { patientId } = event.pathParameters;
+
+    const data = await dynamoDb
+      .get({
+        //select from where id
+        ...params,
+        Key: {
+          patient_id: patientId,
+        },
+      })
+      .promise();
+
+    if (!data.Item) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ error: "Patient not found" }, null, 2),
+      };
+    }
+    const patient = data.Item;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(patient, null, 2),
+    };
+  } catch (err) {
+    console.log("Error", err);
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
+  }
+};
+
+module.exports.postPatient = async (event) => {
+  try {
+    const timestamp = new Date().getTime();
+
+    let data = JSON.parse(event.body);
+
+    const { name, birthdate, email, phoneNumber } = data;
+
+    const patient = {
+      patientId: uuidv4(),
+      name,
+      birthdate,
+      email,
+      phoneNumber,
+      status: true,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    };
+
+    await dynamoDb
+      .put({
+        TableName: "PATIENTS",
+        Item: patient,
+      })
+      .promise();
+
+    return {
+      statusCode: 201,
+    };
+  } catch (err) {
+    console.log("Error", err);
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
+  }
+};
+
+module.exports.updatePatient = async (event) => {
+  const { patientId } = event.pathParameters;
+
+  try {
+    const timestamp = new Date().getTime();
+
+    let data = JSON.parse(event.body);
+
+    const { name, birthdate, email, phoneNumber } = data;
+
+    await dynamoDb
+      .update({
+        ...params,
+        Key: {
+          patient_id: patientId,
+        },
+        UpdateExpression:
+          "SET nome = :name, birthdate = :dt, email = :email," +
+          " phoneNumber = :phoneNumber, updatedAt = :updatedAt",
+        ConditionExpression: "attribute_exists(patient_id)",
+        ExpressionAttributeValues: {
+          ":name": name,
+          ":bd": birthdate,
+          ":email": email,
+          ":phoneNumber": phoneNumber,
+          ":updatedAt": timestamp,
+        },
+      })
+      .promise();
+
+    return {
+      statusCode: 204,
+    };
+  } catch (err) {
+    console.log("Error", err);
+
+    let error = err.name ? err.name : "Exception";
+    let message = err.message ? err.message : "Unknown error";
+    let statusCode = err.statusCode ? err.statusCode : 500;
+
+    if (error == "ConditionalCheckFailedException") {
+      error = "This patient does not exists";
+      message = `This ${patientId} does not exists or cant not be updated`;
+      statusCode = 404;
+    }
+
+    return {
+      statusCode,
+      body: JSON.stringify({
+        error,
+        message,
+      }),
+    };
+  }
+};
+
+module.exports.deletePatient = async (event) => {
+  const { patientId } = event.pathParameters;
+
+  try {
+    await dynamoDb
+      .delete({
+        ...params,
+        Key: {
+          patient_id: patientId,
+        },
+        ConditionExpression: "attribute_exists(patient_id)",
+      })
+      .promise();
+
+    return {
+      statusCode: 204,
+    };
+  } catch (err) {
+    console.log("Error", err);
+
+    let error = err.name ? err.name : "Exception";
+    let message = err.message ? err.message : "Unknown error";
+    let statusCode = err.statusCode ? err.statusCode : 500;
+
+    if (error == "ConditionalCheckFailedException") {
+      error = "Patient not found.";
+      message = `The patient with ID ${patientId} does not exist or can't be deleted.`;
+      statusCode = 404;
+    }
+
+    return {
+      statusCode,
+      body: JSON.stringify({
+        error,
+        message,
+      }),
+    };
+  }
+};
